7fd9c726291ed74725d57ffa2162bb2ca64b9f79
RevaiErik <revaierik100@gmail.com>
2023-11-20 11:52:35 +0100

    valami

---
+++ Assets/Scripts/FlyBehaviourScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private float _velocity = 1.5f;
+    [SerializeField] private float _rotationspeed = 10f;
+    
+    private Rigidbody2D _rigidbody2d;
+
+    private void Start()
+    {
+        _rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame || Keyboard.current.upArrowKey.wasPressedThisFrame) 
+        {
+            _rigidbody2d.velocity = Vector2.up * _velocity;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, _rigidbody2d.velocity.y*_rotationspeed);
+    }
+}
